820c80abe71af198fc82e940fe708ffce6f17d4b
vigo <tarek7salamaa@gmail.com>
2025-03-14 20:40:23 +0200

    Some Updates

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php 
+  // Check If User Come From A Request
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Assign Variables
+    $user = $_POST['username'];
+    $email = $_POST['email'];
+    $mas = $_POST['massage'];
+
+    //Creating Array Of Errors
+    $formErrors = array();
+
+    if(strlen($user) < 3) {
+      $formErrors[] = 'User Name Can\'t Be Less Than <strong>3</strong> Chars!';
+    }
+
+    if(strlen($mas) < 10) {
+      $formErrors[] = 'The Massage Must Be Larger Than <strong>10</strong> Chars!';
+    }
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,29 +33,45 @@
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="./css/contact.css">
   </head>
 
   <body>
     <div class="container">
       <h1 class="text-center mt-3">Contact Me</h1>
-      <form action="">
+      <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+        <?php if(!empty($formErrors)) { ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <?php 
+            $count = 1;
+            foreach( $formErrors as $error ) { 
+              echo $count . ' - '. $error . '<br>';
+              $count++;
+            }
+          ?>
+        </div>
+        <?php } ?>
+
+
 
         <div class="mb-3">
           <!-- <label for="usernameInbut">Username</label> -->
-          <input class="form-control"  type="text" name="username" placeholder="Your Username" required>
+          <input class="form-control"  type="text" name="username" placeholder="Your Username" value="<?php if(isset($user)) {echo $user;} ?>" required>
         </div>
 
         <div class="mb-3">
           <!-- <label for="emailInbut">Email</label> -->
-          <input class="form-control"  type="email" name="email" placeholder="Your Email"  aria-describedby="emailHelp" required>
+          <input class="form-control"  type="email" name="email" placeholder="Your Email" value="<?php if(isset($email)) {echo $email;} ?>"  aria-describedby="emailHelp" required>
           <div id="emailHelp" class="form-text remember">We'll never share your email with anyone else.</div>
         </div>
 
         
         <div class="mb-3">
           <!-- <label for="massageInbut">Your Massage</label> -->
-          <textarea class="form-control"  name="massage" placeholder="Your Massage" required></textarea>
+          <textarea class="form-control"  name="massage" placeholder="Your Massage" required><?php if(isset($mas)) {echo $mas;} ?></textarea>
         </div>
 
         <div class="mb-3">
