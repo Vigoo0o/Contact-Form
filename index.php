941ea6b5139e3a1e6190f5cf9e81dd9b5453da9e
vigo <tarek7salamaa@gmail.com>
2025-08-26 21:00:15 +0300

    complite

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
-<?php 
-  // Check If User Come From A Request
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Assign Variables
-    $user = $_POST['username'];
-    $email = $_POST['email'];
-    $mas = $_POST['massage'];
+<?php
 
-    //Creating Array Of Errors
+  // Check If Come From Request
+  if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Assign Variable
+    $user = filter_var($_POST["username"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $massage = filter_var($_POST["massage"], FILTER_SANITIZE_STRING);
+
+    // Array Of Errors
     $formErrors = array();
+    $errorCount = 0;
 
-    if(strlen($user) < 3) {
-      $formErrors[] = 'User Name Can\'t Be Less Than <strong>3</strong> Chars!';
+    if (strlen($user) < 3) {
+      $formErrors[] = "Username Must Be Larger Than 3 Characters.";
+      $errorCount++;
+    }
+    if (strlen($massage) < 10) {
+      $formErrors[] = "Massage Can't Be Less Than 10 Characters.";
+      $errorCount++;
     }
 
-    if(strlen($mas) < 10) {
-      $formErrors[] = 'The Massage Must Be Larger Than <strong>10</strong> Chars!';
+    // Send Email
+    if ($errorCount === 0) {
+      $header = "From: " . $email . "\n";
+      $myEmail = "t@t.com";
+      $subject = "Contact Form";
+      mail($myEmail, $subject, $massage, $header);
     }
   }
-
 ?>
 
 <!DOCTYPE html>
@@ -36,42 +46,60 @@
     <link rel="stylesheet" href="./css/contact.css">
   </head>
 
+  
+
   <body>
     <div class="container">
       <h1 class="text-center mt-3">Contact Me</h1>
-      <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-        <?php if(!empty($formErrors)) { ?>
+      <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
+        
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+          if ($errorCount > 0) {
+        ?>
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <?php
+            if (isset($formErrors)) {
+              foreach ($formErrors as $error) {
+                echo "- " .  $error . "<br>";
+              }
+            }
+          ?>
           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <?php 
-            $count = 1;
-            foreach( $formErrors as $error ) { 
-              echo $count . ' - '. $error . '<br>';
-              $count++;
-            }
-          ?>
         </div>
-        <?php } ?>
+        <?php
+          } else if ($errorCount === 0) {
+            ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            Success, Send Successfuly
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <?php
+          } 
+        }
+        ?>
 
 
 
         <div class="mb-3">
           <!-- <label for="usernameInbut">Username</label> -->
-          <input class="form-control"  type="text" name="username" placeholder="Your Username" value="<?php if(isset($user)) {echo $user;} ?>" required>
+          <input class="form-control"  type="text" name="username" placeholder="Your Username" value="<?php if (isset($errorCount) && $errorCount > 0)echo $user ?>"  required>
         </div>
 
         <div class="mb-3">
           <!-- <label for="emailInbut">Email</label> -->
-          <input class="form-control"  type="email" name="email" placeholder="Your Email" value="<?php if(isset($email)) {echo $email;} ?>"  aria-describedby="emailHelp" required>
+          <input class="form-control"  type="email" name="email" placeholder="Your Email" value="<?php if (isset($errorCount) && $errorCount > 0)echo $email ?>"  aria-describedby="emailHelp" required>
           <div id="emailHelp" class="form-text remember">We'll never share your email with anyone else.</div>
         </div>
 
         
         <div class="mb-3">
           <!-- <label for="massageInbut">Your Massage</label> -->
-          <textarea class="form-control"  name="massage" placeholder="Your Massage" required><?php if(isset($mas)) {echo $mas;} ?></textarea>
+          <textarea class="form-control"  name="massage" placeholder="Your Massage" required><?php if (isset($errorCount) && $errorCount > 0)echo $massage ?></textarea>
         </div>
 
         <div class="mb-3">
@@ -82,6 +110,7 @@
     </div>
     <script src="js/jQuery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    
   </body>
 
 </html>
